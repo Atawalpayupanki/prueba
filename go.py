b3f72ea23c6e59c64dc0bcf46a36eecdfd228278
Pau <pautall667@gmail.com>
2025-04-09 16:51:09 +0200

    first commit

---
+++ go.py
@@ -0,0 +1,25 @@
+import subprocess
+import os
+import sys
+
+# Nombre del entorno virtual
+env_name = ".venv"
+
+# Crear el entorno virtual
+subprocess.run([sys.executable, "-m", "venv", env_name])
+print(f"Entorno virtual '{env_name}' creado.")
+
+activate_script = os.path.join(env_name, "Scripts", "activate")
+
+subprocess.run([sys.executable, "-m", "venv", env_name])
+repositorio="prueba"
+comandos = [
+    ["git", "init"],
+    ["git", "add", "."],
+    ["git", "commit", "-m", "first commit"],
+    ["git", "branch", "-M", "main"],
+    ["git", "remote", "add", "origin", f"https://github.com/Atawalpayupanki/{repositorio}"],
+    ["git", "push", "-u", "origin", "main"]
+]
+for comando in comandos:
+    subprocess.run(comando)
